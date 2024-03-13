fe256eb6e27ca35c33cadfc2e0056deaa7abbaa6
MANIAC <maniac@pop-os.localdomain>
2024-03-14 01:22:15 +0530

    message

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" to="#">Navbar</a>
+          <NavLink className="navbar-brand" to="/">Beakball</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
