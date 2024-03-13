977f571fbc4fff7d31a574f6d6f133ba23910136
MANIAC <maniac@pop-os.localdomain>
2024-03-14 03:40:48 +0530

    message

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import Logo from "../assets/Logo.webp";
 const Nav = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <NavLink className="navbar-brand" to="/">Beakball</NavLink>
+        <div className="container-fluid" style={{fontWeight:"bold"}}>
+          <img src={Logo} style={{width:"40px",height:"auto"}}/>
+          <NavLink className="navbar-brand" to="/" style={{paddingLeft:"10px"}}>Beakball</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div className="collapse navbar-collapse" id="navbarNav" style={{display:"flex",justifyContent:"right"}}>
             <ul className="navbar-nav">
               <li className="nav-item">
                 <NavLink className="nav-link active" aria-current="page" exact to="/">Home</NavLink>
@@ -24,7 +26,7 @@ const Nav = () => {
                 <NavLink className="nav-link" to="/portfolio">Portfolio</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/contact">Contact Us</NavLink>
+                <NavLink className="nav-link" to="/contact" style={{borderRadius:"30px",backgroundColor:"blue",color:"white",width:"max-content"}}>Contact Us</NavLink>
               </li>
             </ul>
           </div>
