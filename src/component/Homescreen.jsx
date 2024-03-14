45338f8312eba23a8a1240145c3f08be39c8315f
MANIAC <maniac@pop-os.localdomain>
2024-03-15 03:03:27 +0530

    message

--- src/component/Homescreen.jsx
+++ src/component/Homescreen.jsx
@@ -14,7 +14,15 @@ const Homescreen = () => {
   return (
     <div>
       <div className={styles.section1}>
-        <div style={{ position: "absolute", height: "100vh", width: "100%", zIndex: 2, fontSize:"25px", color:"white", display:"flex", alignItems:"center",justifyContent:"center" }}>Hello, Welcome to BeakBall</div>
+        <div style={{ position: "absolute", height: "100vh", width: "100%", zIndex: 2, fontSize:"25px", color:"white", display:"flex", alignItems:"center",justifyContent:"center" }}>
+          <div style={{width:"100%",height:"auto",padding:"40px",marginTop:"300px"}}>
+            <div style={{color:"white", fontSize:"30px",fontWeight:"bold"}}>Beakball: Interactive 3D Virtual Reality</div>
+            <div style={{color:"white", fontSize:"30px",fontWeight:"bold"}}>Unlock New Dimensions</div>
+            <div style={{color:"white", fontSize:"30px",fontWeight:"bold"}}>Where Your Brand’s Vision Meets Virtual Reality</div>
+            <div style={{fontSize:"19px", marginTop:"20px"}}>Secure the competitive advantages for your business by walking ahead of your peers. Impart the capability of 3D modeling and virtual reality into your online presence.</div>
+            <div style={{backgroundColor:"blue",borderRadius:"20px",fontSize:"15px",color:"white",width:"fit-content",paddingLeft:"10px",paddingRight:"10px",paddingTop:"7px",paddingBottom:"7px", marginTop:"5px"}}>DEMO TOUR</div>
+          </div>
+        </div>
         <video autoPlay loop muted controls={false}>
           <source src={backgroundVideo} type="video/mp4" />
           Your browser does not support the video tag.
