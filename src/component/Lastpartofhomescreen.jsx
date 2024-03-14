56ce31dae186bc855f82110545fe45f1c2eb40fd
MANIAC <maniac@pop-os.localdomain>
2024-03-15 01:30:34 +0530

    message

--- src/component/Lastpartofhomescreen.jsx
+++ src/component/Lastpartofhomescreen.jsx
@@ -4,19 +4,20 @@ import Logo from '../assets/Logo.webp';
 const Lastpartofhomescreen = () => {
     return (
         <>
-        <div className={styles.bg}>
-        </div>
+            {/* <div className={styles.bg}>
+            </div> */}
+            <div className={styles.bg} style={{ width: "100%",overflow: "hidden" }}>
+                <div className={styles.box}>
+                    <div className={styles.text}>“What you can imagine,<br />
+                        We bring it to you”</div>
+                    <div className={styles.logo}>
+                        <img className={styles.img} src={Logo} />
+                        <div className={styles.company}>Beakball</div>
+                    </div>
 
-<div className={styles.box}>
-<div className={styles.text}>“What you can imagine,<br />
-    We bring it to you”</div>
-<div className={styles.logo}>
-    <img className={styles.img} src={Logo} />
-    <div className={styles.company}>Beakball</div>
-</div>
-
-</div>
-</>
+                </div>
+            </div>
+        </>
     )
 }
 
