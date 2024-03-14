0b55178945840a1c517d01e65b228c637e6fe755
MANIAC <maniac@pop-os.localdomain>
2024-03-14 21:00:18 +0530

    message

--- src/component/Why.jsx
+++ src/component/Why.jsx
@@ -11,8 +11,8 @@ const Why = () => {
 
             <div className={style.contentpart}>
             <div className={styles.flexbox}>
-                <div className={styles.part1} style={{fontSize:"35px",padding:"15px",width:"600px",border:"2px solid green"}}>Pioneering Transformative Technologies to Explore New Dimensions of Endless Opportunities </div>
-                <div className={styles.part2} style={{fontSize:"20px",padding:"15px",width:"600px",border:"2px solid green"}}>Beakball comes with a landscape reshaping concept that invites the business to transform their existing websites with some creative and engaging virtual tours. Whether you want to present your entire premises or a digital replica of a product to your target audience, our 3D Modeling and Virtual Tour gets you covered. We aim to change the way people engage, learn, and perceive the digital realm. </div>
+                <div className={styles.part1} style={{fontSize:"35px",padding:"5px",width:"630px",border:"2px solid green"}}>Pioneering Transformative Technologies to Explore New Dimensions of Endless Opportunities </div>
+                <div className={styles.part2} style={{fontSize:"20px",padding:"5px",width:"630px",border:"2px solid green"}}>Beakball comes with a landscape reshaping concept that invites the business to transform their existing websites with some creative and engaging virtual tours. Whether you want to present your entire premises or a digital replica of a product to your target audience, our 3D Modeling and Virtual Tour gets you covered. We aim to change the way people engage, learn, and perceive the digital realm. </div>
             </div>
             </div>
         </div>
