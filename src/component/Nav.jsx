86da98b86ea8fb604063d531f610b479698ff718
MANIAC <maniac@pop-os.localdomain>
2024-03-14 20:20:25 +0530

    massage

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid" >
+        <div className="container-fluid" style={{fontWeight:"bold"}}>
           <img src={Logo} style={{width:"40px",height:"auto"}}/>
           <NavLink className="navbar-brand" to="/" style={{paddingLeft:"10px"}}>Beakball</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,7 +26,7 @@ const Nav = () => {
                 <NavLink className="nav-link" to="/portfolio" style={{margin:"10px"}}>PORTFOLIO</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/contact" style={{borderRadius:"30px",backgroundColor:"blue",color:"white",width:"max-content",margin:"10px"}}>CONTACT US</NavLink>
+                <NavLink className="nav-link" to="/contact" style={{borderRadius:"10px",backgroundColor:"blue",color:"white",width:"max-content",margin:"10px"}}>CONTACT US</NavLink>
               </li>
             </ul>
           </div>
