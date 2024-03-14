76bd60ae03c96c99e37017dcb546c47d9d2e8eeb
MANIAC <maniac@pop-os.localdomain>
2024-03-14 20:19:03 +0530

    massage

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid" style={{fontWeight:"bold"}}>
+        <div className="container-fluid" >
           <img src={Logo} style={{width:"40px",height:"auto"}}/>
           <NavLink className="navbar-brand" to="/" style={{paddingLeft:"10px"}}>Beakball</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,19 +14,19 @@ const Nav = () => {
           <div className="collapse navbar-collapse" id="navbarNav" style={{display:"flex",justifyContent:"right"}}>
             <ul className="navbar-nav">
               <li className="nav-item">
-                <NavLink className="nav-link active" aria-current="page" exact to="/" style={{margin:"10px"}}>Home</NavLink>
+                <NavLink className="nav-link active" aria-current="page" exact to="/" style={{margin:"10px"}}>HOME</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/about" style={{margin:"10px"}}>About</NavLink>
+                <NavLink className="nav-link" to="/about" style={{margin:"10px"}}>ABOUT</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/service" style={{margin:"10px"}}>Service</NavLink>
+                <NavLink className="nav-link" to="/service" style={{margin:"10px"}}>SERVICE</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/portfolio" style={{margin:"10px"}}>Portfolio</NavLink>
+                <NavLink className="nav-link" to="/portfolio" style={{margin:"10px"}}>PORTFOLIO</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/contact" style={{borderRadius:"30px",backgroundColor:"blue",color:"white",width:"max-content",margin:"10px"}}>Contact Us</NavLink>
+                <NavLink className="nav-link" to="/contact" style={{borderRadius:"30px",backgroundColor:"blue",color:"white",width:"max-content",margin:"10px"}}>CONTACT US</NavLink>
               </li>
             </ul>
           </div>
