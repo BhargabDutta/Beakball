2295595ae3186ba681613739a6af87b51d86f670
MANIAC <maniac@pop-os.localdomain>
2024-03-17 00:59:30 +0530

    message

--- src/component/Portfoliogrid.jsx
+++ src/component/Portfoliogrid.jsx
@@ -14,52 +14,52 @@ export default function Portfoliogrid() {
         <div style={{overflow:"hidden",paddingTop:"40px"}}>
             <div style={{fontSize:"30px",padding:"23px",fontWeight:"bold"}}>Our Projects</div>
             <hr style={{width:"96%",borderColor:"blue",borderWidth:"5px", margin:"0 auto"}}/>
-            <MDBRow >
-                <MDBCol md='2' style={{ width: "300px",margin:"23px"}}>
+            <MDBRow style={{display:"flex",alignItems:"start",justifyContent:"space-around",marginTop:"30px"}}>
+                <MDBCol md='2' style={{ width: "300px",marginTop:"50px"}}>
                     <Portfoliogrid_card 
                     img={card1}
                     title="Hotel "
                     content="Experience the luxury of this place from your screen. Catch everything you will get here before paying. 
                     "/>
                 </MDBCol>
-                <MDBCol md='2' style={{ width: "300px",margin:"23px"}}>
+                <MDBCol md='2' style={{ width: "300px",marginTop:"50px"}}>
                     <Portfoliogrid_card 
                     img={card2}
                     title="University and College"
                     content="Explore the campus of an University virtually. Find your departments and map of the college.  "/>
                 </MDBCol>
-                <MDBCol md='2' style={{ width: "300px",margin:"23px"}}>
+                <MDBCol md='2' style={{ width: "300px",marginTop:"50px"}}>
                     <Portfoliogrid_card 
                     img={card3}
                     title="Home "
                     content="Experience your modern home where you will find your permanent with virtual reality."/>
                 </MDBCol>
-                <MDBCol md='2' style={{ width: "300px",margin:"23px"}}>
+                <MDBCol md='2' style={{ width: "300px",marginTop:"50px"}}>
                     <Portfoliogrid_card 
                     img={card4}
                     title="Cinema Hall"
                     content="Your digital space where you want to promote your services first hand. Create your virtual world with AI."/>
                 </MDBCol>
 
-                <MDBCol md='2' style={{ width: "300px",margin:"23px"}}>
+                <MDBCol md='2' style={{ width: "300px",marginTop:"50px"}}>
                     <Portfoliogrid_card 
                     img={card5}
                     title="Movie Ticket"
                     content="Grab more attention at your marketing by creating engaging content published in Virtaul world. "/>
                 </MDBCol>
-                <MDBCol md='2' style={{ width: "300px",margin:"23px"}}>
+                <MDBCol md='2' style={{ width: "300px",marginTop:"50px"}}>
                     <Portfoliogrid_card 
                     img={card6}
                     title="3d Products"
                     content="Before you buy a product, experience the product shape in the virtual world. "/>
                 </MDBCol>
-                <MDBCol md='2' style={{ width: "300px",margin:"23px"}}>
+                <MDBCol md='2' style={{ width: "300px",marginTop:"50px"}}>
                     <Portfoliogrid_card 
                     img={card7}
                     title="Tourism "
                     content="Experience destinations and places in virtual world. Grab more attention from the tourist beforehand. "/>
                 </MDBCol>
-                <MDBCol md='2' style={{ width: "300px",margin:"23px"}}>
+                <MDBCol md='2' style={{ width: "300px",marginTop:"50px"}}>
                     <Portfoliogrid_card 
                     img={card8}
                     title="3D models"
