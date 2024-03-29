c12427c6a1a75a244b0b64396e22275d3ba99bf8
MANIAC <maniac@pop-os.localdomain>
2024-03-30 02:31:57 +0530

    message

--- src/component/Homescreen.jsx
+++ src/component/Homescreen.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LazyLoad from 'react-lazyload';
 import styles from '../styles/Homescreen.module.css';
 import Contactus from './Contactus';
 import Why from './Why';
@@ -13,46 +14,50 @@ import Successpt2 from './Successpt2';
 import Successpt3 from './Successpt3';
 import responsive from "../styles/responsive.module.css";
 const Homescreen = () => {
+//   const isHighResolution = window.matchMedia("(min-resolution: 2dppx)").matches;
+// const videoSrc = isHighResolution ? highQualityVideo : lowQualityVideo;
   return (
     <div>
       <div className={responsive.home_hero_desktop}>
-      <div className={styles.section1}>
-        <div style={{ position: "absolute", height: "100vh", width: "100%", zIndex: 2, fontSize: "25px", color: "white", display: "flex", alignItems: "center", justifyContent: "center"}}>
-          <div style={{ width: "100%", height: "auto", padding: "80px", marginTop: "150px"}}>
-            <div style={{ color: "white", fontSize: "30px", fontWeight: "bold" }}>Beakball: Interactive 3D Virtual Reality</div>
-            <div style={{ color: "white", fontSize: "30px", fontWeight: "bold" }}>Unlock New Dimensions</div>
-            <div style={{ color: "white", fontSize: "30px", fontWeight: "bold" }}>Where Your Brand’s Vision Meets Virtual Reality</div>
-            <div style={{ fontSize: "19px", marginTop: "20px" }}>Secure the competitive advantages for your business by walking ahead of your peers. Impart the capability of 3D modeling and virtual reality into your online presence.</div>
-            <a href='https://banner.beakball.com/' target='_blank'>
-            <div style={{ backgroundColor: "blue", borderRadius: "10px", fontSize: "20px", color: "white", width: "fit-content", paddingLeft: "10px", paddingRight: "10px", paddingTop: "7px", paddingBottom: "7px", marginTop: "20px", fontWeight: "bold",cursor:"pointer"}}>Demo Tour</div></a>
+        <div className={styles.section1}>
+          <div style={{ position: "absolute", height: "100vh", width: "100%", zIndex: 2, fontSize: "25px", color: "white", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <div style={{ width: "100%", height: "auto", padding: "80px", marginTop: "150px" }}>
+              <div style={{ color: "white", fontSize: "30px", fontWeight: "bold" }}>Feels Like Real Because It Is Real</div>
+              {/* <div style={{ color: "white", fontSize: "30px", fontWeight: "bold" }}>Unlock New Dimensions</div>
+            <div style={{ color: "white", fontSize: "30px", fontWeight: "bold" }}>Where Your Brand’s Vision Meets Virtual Reality</div> */}
+              <div style={{ fontSize: "19px", marginTop: "20px" }}>Explore a Virtual Reality world where you can feel everything right in front of you, literally from anywhere.</div>
+              <a href='https://banner.beakball.com/' target='_blank'>
+                <div style={{ backgroundColor: "blue", borderRadius: "10px", fontSize: "20px", color: "white", width: "fit-content", paddingLeft: "10px", paddingRight: "10px", paddingTop: "7px", paddingBottom: "7px", marginTop: "20px", fontWeight: "bold", cursor: "pointer" }}>Demo Tour</div></a>
+            </div>
           </div>
-        </div>
-        <video autoPlay loop muted controls={false} style={{width:"100%",height:"100vh",objectFit:"cover"}}>
-          <source src={backgroundVideo} type="video/mp4"/>
-          Your browser does not support the video tag.
-        </video>
+          <LazyLoad>
+            <video autoPlay loop muted controls={false} style={{ width: "100%", height: "100vh", objectFit: "cover" }} playsinline webkit-playsinline>
+              <source src={backgroundVideo} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </LazyLoad>
 
-      </div>
+        </div>
       </div>
 
       <div className={responsive.home_hero_mobile}>
-      <div className={styles.section1}>
-        <div style={{ position: "absolute", height: "100vh", width: "100%", zIndex: 2, fontSize: "25px", color: "white", display: "flex", alignItems: "center", justifyContent: "center"}}>
-          <div style={{ width: "100%", height: "auto", padding: "20px", marginTop: "0px"}}>
-            <div style={{ color: "white", fontSize: "25px", fontWeight: "bold" ,paddingTop:"20px"}}>Beakball: Interactive 3D Virtual Reality</div>
-            <div style={{ color: "white", fontSize: "20px", fontWeight: "bold" ,paddingTop:"20px"}}>Unlock New Dimensions</div>
-            <div style={{ color: "white", fontSize: "20px", fontWeight: "bold" ,paddingTop:"20px"}}>Where Your Brand’s Vision Meets Virtual Reality</div>
-            <div style={{ fontSize: "15px", marginTop: "20px" }}>Secure the competitive advantages for your business by walking ahead of your peers. Impart the capability of 3D modeling and virtual reality into your online presence.</div>
-            <a href='https://banner.beakball.com/' target='_blank'>
-            <div style={{ backgroundColor: "blue", borderRadius: "5px", fontSize: "20px", color: "white", width: "fit-content", paddingLeft: "8px", paddingRight: "8px", paddingTop: "2px", paddingBottom: "2px", marginTop: "10px", fontWeight: "bold" }}>Demo Tour</div></a>
+        <div className={styles.section1}>
+          <div style={{ position: "absolute", height: "100vh", width: "100%", zIndex: 2, fontSize: "25px", color: "white", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <div style={{ width: "100%", height: "auto", padding: "20px", marginTop: "0px" }}>
+              <div style={{ color: "white", fontSize: "25px", fontWeight: "bold", paddingTop: "20px" }}>Beakball: Interactive 3D Virtual Reality</div>
+              <div style={{ color: "white", fontSize: "20px", fontWeight: "bold", paddingTop: "20px" }}>Unlock New Dimensions</div>
+              <div style={{ color: "white", fontSize: "20px", fontWeight: "bold", paddingTop: "20px" }}>Where Your Brand’s Vision Meets Virtual Reality</div>
+              <div style={{ fontSize: "15px", marginTop: "20px" }}>Secure the competitive advantages for your business by walking ahead of your peers. Impart the capability of 3D modeling and virtual reality into your online presence.</div>
+              <a href='https://banner.beakball.com/' target='_blank'>
+                <div style={{ backgroundColor: "blue", borderRadius: "5px", fontSize: "20px", color: "white", width: "fit-content", paddingLeft: "8px", paddingRight: "8px", paddingTop: "2px", paddingBottom: "2px", marginTop: "10px", fontWeight: "bold" }}>Demo Tour</div></a>
+            </div>
           </div>
-        </div>
-        <video autoPlay loop muted controls={false} style={{ height: '100vh', width: '100%', objectFit: 'cover' }}>
-          <source src={backgroundVideo} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+          <video autoPlay loop muted controls={false} style={{ height: '100vh', width: '100%', objectFit: 'cover' }} playsinline webkit-playsinline>
+            <source src={backgroundVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
 
-      </div>
+        </div>
       </div>
 
       <div className={styles.section2}>
