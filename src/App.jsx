7a4635c737d3e26aa1e066e8708b1764e56aa2e5
MANIAC <maniac@pop-os.localdomain>
2024-03-31 00:29:18 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./App.css";
-
-// import Servicepage_pt2 from '../src/component/Servicepage_pt2';
 import Homescreen from './component/Homescreen';
 import Contactpage from '../src/component/Contactpage';
 import Portfoliopage from './component/Portfoliopage';
 import Footer from './component/Footer';
-// import NavBar from './component/Navfolder/NavBar';
 import Nav from './component/Nav';
 import Servicepage from './component/Servicepage';
-// import Pagee from "./component/Pagee";
-// import { Route, BrowserRouter , Routes} from 'react-router-dom';
-import Successpt2 from './component/Successpt2';
-import Successpt3 from './component/Successpt3';
 import Faq from './component/Faq';
 import About from './component/About';
 import Privacy from './component/Privacy';
