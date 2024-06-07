e3f32d78634eec6728dfd717042e4ecc89e2312a
MANIAC <maniac@pop-os.localdomain>
2024-06-07 20:33:44 +0530

    final

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -26,10 +26,10 @@ export default function App() {
   return (
     <MDBNavbar expand='lg' light bgColor='#ff000080' style={{position:"fixed",zIndex:"4",width:"100vw",height:"55px"}}>
       <MDBContainer fluid style={{backgroundColor:"#00000080",backdropFilter:"blur(10px)",paddingTop:"10px",paddingBottom:"10px"}}>
-      <MDBNavbarBrand href='#' style={{color:"white",fontWeight:"bold",fontWeight:"bold",gap:"10px"}}>
+      <MDBNavbarBrand href='#' style={{color:"white",fontWeight:"bold",gap:"10px"}}>
             <img
               src='https://www.beakball.com/assets/Logo-CjjAgmgS.webp'
-              height='50'
+              height='40'
               alt=''
             />Beakball</MDBNavbarBrand>
         <MDBNavbarToggler
